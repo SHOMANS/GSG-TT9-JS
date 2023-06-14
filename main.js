6f83695539a1ed63656a6be7f3d786f5c16536eb
SHOMANS <mohammedshoman9@gmail.com>
2023-06-15 01:40:01 +0300

    finish session 5

--- main.js
+++ main.js
@@ -1,179 +1,173 @@
-// const usersArray = [
-//   {
-//     name: 'Hazem',
-//     age: 30,
-//     gender: 'male',
-//     id: 1,
-//   },
-//   {
-//     name: 'Amro',
-//     age: 20,
-//     gender: 'male',
-//     id: 2,
-//   },
-//   {
-//     name: 'Hamza',
-//     age: 36,
-//     gender: 'male',
-//     id: 3,
-//   },
-//   {
-//     name: 'Omer',
-//     age: 25,
-//     gender: 'male',
-//     id: 4,
-//   },
-// ];
+// const myButton = document.getElementById('click');
 
-// const months = ['Jan', 'Feb', 'March', 'April', 'June'];
+// myButton.addEventListener('click', () => console.log('clicked from add event listener'));
+// myButton.addEventListener('click', () => console.log('clicked from add event listener2'));
+// myButton.addEventListener('click', () => console.log('clicked from add event listener2'));
+// myButton.addEventListener('click', () => console.log('clicked from add event listener2'));
+// myButton.addEventListener('click', () => console.log('clicked from add event listener2'));
+// myButton.addEventListener('click', () => console.log('clicked from add event listener2'));
 
-// // console.log(months[0]);
+// myButton.onclick = () => console.log('clicked from onClick');
+// myButton.onclick = () => console.log('clicked from onClick2');
+// myButton.onclick = () => null;
 
-// const mySecondFunction = (arr) => {
-//   arr.forEach((element) => {
-//     console.log(element);
+// --------------------------------------------------------
+
+// const btn1 = document.getElementById('one');
+// const btn2 = document.getElementById('two');
+// const btn3 = document.getElementById('three');
+// const btn4 = document.getElementById('four');
+
+// const content = document.getElementById('content');
+
+// btn1.addEventListener('click', () => {
+//   content.innerHTML = 'Button 1 clicked';
+// });
+
+// btn2.addEventListener('click', () => {
+//   content.innerHTML = 'Button 2 clicked';
+// });
+
+// btn3.addEventListener('click', () => {
+//   content.innerHTML = 'Button 3 clicked';
+// });
+
+// btn4.addEventListener('click', () => {
+//   content.innerHTML = 'Button 4 clicked';
+// });
+
+// --------------------------------------------------------
+
+// const contentArr = ['Button 1 clicked', 'Button 2 clicked', 'Button 3 clicked', 'Button 4 clicked'];
+
+// const content = document.getElementById('content');
+
+// const myBtns = document.querySelectorAll('#btns button');
+
+// console.log(myBtns);
+
+// myBtns.forEach((btn, index) => {
+//   btn.addEventListener('click', () => {
+//     content.innerHTML = contentArr[index];
 //   });
+// });
+
+// --------------------------------------------------------
+
+// const contentObj = {
+//   one: 'Button 1 clicked',
+//   two: 'Button 2 clicked',
+//   three: 'Button 3 clicked',
+//   four: 'Button 4 clicked',
 // };
 
-// const myFunction = (arr) => {
-//   const [first, second, ...rest] = arr; // destructuring
+// const myBtns = document.querySelectorAll('#btns button');
 
-//   mySecondFunction(rest);
+// console.log(myBtns);
+
+// myBtns.forEach((btn) => {
+//   btn.addEventListener('click', () => {
+//     content.innerHTML = contentObj[btn.id];
+//   });
+// });
+
+// --------------------------------------------------------
+
+// const contentObj = {
+//   one: 'Button 1 clicked',
+//   two: 'Button 2 clicked',
+//   three: 'Button 3 clicked',
+//   four: 'Button 4 clicked',
 // };
 
-// myFunction(usersArray);
-// // console.log(months);
+// const btnDev = document.getElementById('btns');
+// const content = document.getElementById('content');
 
-// const q1 = ['Jan', 'Feb', 'Mar'];
-// const q2 = ['Apr', 'May', 'Jun'];
-// const q3 = ['Jul', 'Aug', 'Sep'];
-// const q4 = ['Oct', 'Nov', 'May'];
+// Object.keys(contentObj).forEach((item) => {
+//   const btn = document.createElement('button');
 
-// const year = [[...q1], q2, q3, q4];
-// const year = [q1, q2, q3, q4];
+//   const btnContent = document.createTextNode(item);
 
-// console.log(year);
+//   btn.appendChild(btnContent);
 
-// console.log(year[0] === q1);
+//   btnDev.appendChild(btn);
 
-// --------------------------------------------------
+//   btn.addEventListener('click', () => {
+//     content.textContent = contentObj[item];
+//   });
+// });
 
-// const smallObj = {
-//   test: 'Hazem',
-// };
+// --------------------------------------------------------
+// --------------------------------------------------------
+// --------------------------------------------------------
+// --------------------------------------------------------
+// --------------------------------------------------------
+
+// const form = document.querySelector('form');
+// const fname = document.getElementById('fname');
+// const lname = document.getElementById('lname');
+// const para = document.querySelector('p');
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   console.log(e.target.id);
+//   if (e.target.fname.value === '' || e.target.lname.value === '') {
+//     para.textContent = 'You need to fill in both names!';
+//   }
+// });
+
+// fname.addEventListener('input', (e) => {
+//   console.log(e.target.value);
+//   if (e.target.value.length > 8) {
+//     e.target.disabled = 'disabled';
+//     e.target.style.border = '1px solid red';
+
+//     para.style.color = 'red';
+//   } else {
+//     para.textContent = 8 - e.target.value.length;
+//   }
+// });
+
+// --------------------------------------------------------
+
+// console.log(window);
+
+// const myInterval = setInterval(() => {
+//   console.log('done');
+// }, 1000);
+
+// setTimeout(() => {
+//   clearInterval(myInterval);
+// }, 10000);
+
+// --------------------------------------------------------
+
+// localStorage.setItem('theme', 'dark');
+
+// console.log(localStorage.getItem('theme'));
+
+// localStorage.clear()
+// localStorage.removeItem('theme');
+
+// --------------------------------------------------------
+
+// sessionStorage.setItem('theme', 'dark');
+
+// console.log(sessionStorage.getItem('theme'));
+
+// sessionStorage.clear();
+// sessionStorage.removeItem('theme');
+
+// --------------------------------------------------------
 
 // const obj = {
-//   name: 'Hazem',
-//   age: 30,
-//   address: 'Khan',
+//   name: 'hazem',
 // };
 
-// const newObj = { obj: { ...obj }, ...smallObj };
+// const myObj = JSON.stringify(obj); // to string
 
-// console.log(newObj.obj === obj);
+// localStorage.setItem('data', myObj);
 
-// const { ...rest } = obj;
+// console.log(JSON.parse(localStorage.getItem('data')));
 
-// const newObj = { ...obj };
-
-// /**
-//  * by writing {} it will create new object with new reference
-//  * then by adding ...obj inside the {} it will deep copying the data from obj and storage them inside the newObj
-//  */
-
-// console.log(newObj === obj);
-
-// --------------------------------------------------
-
-// function myFunction(x, y = 10) {
-//   // y is 10 if not passed or undefined
-//   return x + y;
-// }
-// console.log(myFunction(5)); // will return 15
-
-// --------------------------------------------------
-// function sum() {
-//   let sum = 0;
-//   for (let arg of arguments) sum += arg;
-//   return sum;
-// }
-
-// const sum = (...args) => {
-//   let sum = 0;
-//   for (let arg of args) sum += arg;
-//   return sum;
-// };
-
-// let x = sum(4, 9, 16, 25, 29, 100, 66, 77);
-// console.log(sum(x));
-// --------------------------------------------------
-// console.log(9999999999999999); // will print 10000000000000000
-// console.log(Number.isSafeInteger(999999999999999)); // true
-// console.log(Number.isSafeInteger(9999999999999999)); // false
-
-// let x = 10;
-
-// x **= 2;
-// console.log(x);
-
-// --------------------------------------------------
-// --------------------------------------------------
-// --------------------------------------------------
-
-const arr2 = [1]; // 1
-
-const arr3 = []; // 1
-
-// const arr4 = [1, 2, [3, 4, [5], 6, [[[7], 19], [8, [9], [10, [11]]], 12], [[13]], 14], 15]; // 6
-
-const arr5 = [1, [2], 3, [4, [5]], 6]; // 3
-
-/**
- * you should create a function that
- * take an array as parameter with nested arrays
- * and it should return the depth of this arr
- */
-
-// const arr = [1, 2, [3, [4, [5]], [6]]]; // 4
-
-// const arr4 = [1, 2, [3, 4, [5], 6, [[[7], 19], [8, [9], [10, [11]]], 12], [[13]], 14], 15]; // 6
-
-// const calculateDepth = (arr, maxDepth = 0) => {
-//   if (Array.isArray(arr)) {
-//     arr.forEach((item) => {
-//       maxDepth = Math.max(calculateDepth(item), maxDepth);
-//     });
-
-//     return maxDepth + 1;
-//   }
-
-//   return maxDepth;
-// };
-
-// const calculateArrayDepth = (arr) => (Array.isArray(arr) ? 1 + Math.max(...arr.map(calculateArrayDepth)) : 0);
-
-// console.log(findDepth(arr4));
-
-// const func = (arr) => {
-//   let counter = 0;
-//   let myDepthsArr = [];
-//   const string = JSON.stringify(arr);
-//   for (let i = 0; i < string.length; i++) {
-//     if (string[i] === '[') counter++;
-//     if (string[i] === ']') counter--;
-//     myDepthsArr.push(counter);
-//   }
-
-//   console.log(string);
-//   return Math.max(...myDepthsArr);
-// };
-
-// console.log(func(arr4));
-
-// --------------------------------------------------
-
-// let test = 1;
-
-// test ^= 1;
-
-// console.log(test);
+// --------------------------------------------------------
