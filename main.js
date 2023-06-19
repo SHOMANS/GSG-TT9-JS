915a895700e50a15f210ed72bc74c6912b2b474a
SHOMANS <mohammedshoman9@gmail.com>
2023-06-19 15:02:07 +0300

    finish sesstion 7

---
+++ main.js
@@ -0,0 +1,43 @@
+// const isYes = true;
+
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isYes) {
+//       resolve('Success!');
+//     } else {
+//       reject('Failed!');
+//     }
+//   }, 1000);
+// });
+
+// console.log('first');
+
+// myPromise
+//   .then((value) => {
+//     console.log('second');
+//     return value;
+//   })
+//   .then((value) => console.log('second value', value))
+//   .catch((err) => console.log(err));
+
+// console.log('third');
+
+// const myOuterFunction = () => {
+//   const myInnerFunction = () => {
+//     console.log('Hello from inner function');
+//   };
+//   myInnerFunction();
+//   console.log('Hello from outer function');
+// };
+
+// myOuterFunction();
+
+/**
+ * myOuterFunction
+ * myInnerFunction
+ * console.log
+ *
+ */
+
+// last in first out LIFO (stack)
+// first in first out FIFO (queue)
